d305824479bf9e298a69a837619a05b6832493ac
Ghazaleh <ghazalehansari1373@gamil.com>
2025-05-05 18:41:21 +0200

    ft_snprinf is used instead of snprintf

--- srcs/parsing/expansion.c
+++ srcs/parsing/expansion.c
@@ -4,18 +4,18 @@ char *process_variable(char *var_name, t_shell *shell)
 {
     if (!var_name || !*var_name)
         return (ft_strdup("$"));
-    
+
     if (ft_strcmp(var_name, "?") == 0)
     {
         char number[12];
-        snprintf(number, sizeof(number), "%d", shell->exit_status);
+        ft_snprintf(number, sizeof(number), "%d", shell->exit_status);
         return (ft_strdup(number));
     }
-    
+
     char *value = ft_getenv(var_name, shell);
     if (value)
         return (ft_strdup(value));
-    
+
     return (ft_strdup(""));
 }
 
@@ -23,19 +23,19 @@ char *extract_var_name(char *str, int *index)
 {
     int start;
     int i;
-    
+
     i = *index + 1;
     start = i;
-    
+
     if (str[i] == '?')
     {
         *index = i;
         return (ft_strdup("?"));
     }
-    
+
     while (str[i] && (ft_isalnum(str[i]) || str[i] == '_'))
         i++;
-    
+
     *index = i - 1;
     return (ft_substr(str, start, i - start));
 }
