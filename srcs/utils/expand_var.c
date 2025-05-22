e87a9fb7cab1057420d26b5999c34d1540730d39
Ghazaleh Ansari <gansari@student.42berlin.de>
2025-05-22 18:08:05 +0200

    echo env variables fixed

--- srcs/utils/expand_var.c
+++ srcs/utils/expand_var.c
@@ -3,14 +3,17 @@
 /*                                                        :::      ::::::::   */
 /*   expand_var.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: mukibrok <mukibrok@student.42.fr>          +#+  +:+       +#+        */
+/*   By: gansari <gansari@student.42berlin.de>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/24 15:11:20 by gansari           #+#    #+#             */
-/*   Updated: 2025/05/20 17:46:31 by mukibrok         ###   ########.fr       */
+/*   Updated: 2025/05/22 17:42:32 by gansari          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../includes/sadaf.h"
+int	is_inside_single_quotes(char *arg, int pos);
+int	is_inside_double_quotes(char *arg, int pos);
+int	is_inside_single_quotes_up_to(char *arg, int pos);
 
 char	*handle_exit_status(char *expanded, t_shell *shell)
 {
@@ -32,15 +35,19 @@ char	*expand_var(char *expanded, char *arg, int *j, t_shell *shell)
 	char	*tmp;
 
 	start = *j + 1;
+	// Check if there's a valid variable name after $
 	if (!arg[start] || (!ft_isalnum(arg[start]) && arg[start] != '_'))
 	{
+		// No valid variable name, just append the $ literally
 		tmp = expanded;
 		expanded = ft_strjoin(expanded, "$");
 		free(tmp);
 		return (expanded);
 	}
+	
 	while (arg[*j + 1] && (ft_isalnum(arg[*j + 1]) || arg[*j + 1] == '_'))
 		(*j)++;
+	
 	if (start <= *j)
 	{
 		var = ft_substr(arg, start, *j - start + 1);
@@ -69,6 +76,54 @@ char	*append_char(char *expanded, char c)
 	return (expanded);
 }
 
+int	is_inside_single_quotes(char *arg, int pos)
+{
+	int		i;
+	int		in_single_quotes;
+
+	i = 0;
+	in_single_quotes = 0;
+	while (i < pos)
+	{
+		if (arg[i] == '\'' && !is_inside_double_quotes(arg, i))
+			in_single_quotes = !in_single_quotes;
+		i++;
+	}
+	return (in_single_quotes);
+}
+
+int	is_inside_double_quotes(char *arg, int pos)
+{
+	int		i;
+	int		in_double_quotes;
+
+	i = 0;
+	in_double_quotes = 0;
+	while (i < pos)
+	{
+		if (arg[i] == '"' && !is_inside_single_quotes_up_to(arg, i))
+			in_double_quotes = !in_double_quotes;
+		i++;
+	}
+	return (in_double_quotes);
+}
+
+int	is_inside_single_quotes_up_to(char *arg, int pos)
+{
+	int		i;
+	int		in_single_quotes;
+
+	i = 0;
+	in_single_quotes = 0;
+	while (i < pos)
+	{
+		if (arg[i] == '\'')
+			in_single_quotes = !in_single_quotes;
+		i++;
+	}
+	return (in_single_quotes);
+}
+
 char	*process_arg(char *arg, t_shell *shell)
 {
 	int		j;
@@ -78,7 +133,7 @@ char	*process_arg(char *arg, t_shell *shell)
 	j = 0;
 	while (arg[j])
 	{
-		if (arg[j] == '$')
+		if (arg[j] == '$' && !is_inside_single_quotes(arg, j))
 		{
 			if (arg[j + 1] == '?')
 			{
